9bec3d972c005e01fe75f58e7bc086201b053a37
Kanya <sKCuA2@kua.local>
2022-03-27 20:57:58 -0700

    created components and testing pages

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TechUser from "./Components/TechUsers/techUser";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">we are live</header>
+      <div>
+        {/* Home page: 
+        Header  - There will be a TechBack - it  will link to the homepage
+        Body - tagline "Where Engineers and the communities intersect"
+        Main section - I am currently ... 
+        There will be two buttons 
+        1) In Tech
+        2) In the Community
+
+        There will be section on the bottom that says
+
+        "Just Browsing" - This will link to the get all page of engineers and community leaders
+     */}
+      </div>
+      <div>
+        <TechUser />
+      </div>
     </div>
   );
 }
---
+++ front_end/src/Components/TechUsers/techUser.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TechUser() {
+  return (
+    <div className="TechUser">
+      
+      <h1>
+          This the techUser information
+      </h1>
+    </div>
+  );
+}
+
+export default TechUser
